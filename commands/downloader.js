f69939125fcd1e0e81e9f68ebde08d267a60dc8e
CYBER DEXTER <152212325+darkewing@users.noreply.github.com>
2024-03-23 00:17:42 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -235,12 +235,12 @@ cmd({
                 caption: `
 ╔═════════•∞•═╗
 │⿻ ${tlang().title} 
-│*Youtube Player* ✨
-│⿻*Title:* ${anu.title}
-│⿻*Duration:* ${anu.timestamp}
-│⿻*Viewers:* ${anu.views}
-│⿻*Uploaded:* ${anu.ago}
-│⿻*Author:* ${anu.author.name}
+│Youtube Player ✨
+│⿻Title: ${anu.title}
+│⿻Duration: ${anu.timestamp}
+│⿻Viewers: ${anu.views}
+│⿻Uploaded: ${anu.ago}
+│⿻Author: ${anu.author.name}
 ╚═•∞•═════════╝
 ⦿*Url* : ${anu.url}
 `,
